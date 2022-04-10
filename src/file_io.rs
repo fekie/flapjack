f56551721bd00281451b2046e05e1a50292f8181
Chloe-Woahie <longwelldotpy@gmail.com>
2022-04-10 13:16:20 -0400

    brb

--- src/file_io.rs
+++ src/file_io.rs
@@ -2,7 +2,7 @@ use dirs;
 use std::fs;
 use std::fs::OpenOptions;
 
-pub fn init_log_db() {
+pub fn init_log_db() -> String {
     let local_data_dir = match dirs::data_local_dir() {
         Some(dir) => dir,
         None => panic!("Could not find local data directory"),
@@ -26,24 +26,29 @@ pub fn init_log_db() {
         .append(true)
         .open(file_path);
 
-    match file {
-        Ok(_) => println!("Created {}", file_path.display()),
+    let path_string = match file {
+        Ok(_) => {
+            println!("Created {}", file_path.display());
+            file_path.to_string_lossy().to_string()
+        }
         Err(e) => match e.kind() {
-            std::io::ErrorKind::AlreadyExists => (),
+            std::io::ErrorKind::AlreadyExists => file_path.to_string_lossy().to_string(),
             _ => panic!("Could not create log_db.flap"),
         },
-    }
+    };
+
+    path_string
 }
 
-pub fn read_raw_db_contents() -> Result<String, std::io::Error> {
-    let local_data_dir = match dirs::data_local_dir() {
+pub fn read_raw_db_contents(path: &str) -> Result<String, std::io::Error> {
+    /* let local_data_dir = match dirs::data_local_dir() {
         Some(dir) => dir,
         None => panic!("Could not find local data directory"),
     };
     let flapjack_data_dir = local_data_dir.join("flapjack");
-    let file_path = &flapjack_data_dir.join("log_db.flap");
+    let file_path = &flapjack_data_dir.join("log_db.flap"); */
 
-    let file = fs::read_to_string(file_path);
+    let file = fs::read_to_string(path);
 
     match file {
         Ok(contents) => Ok(contents),
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 mod file_io;
+mod flapjack_stack;
+
+use clap::Parser;
+use flapjack_stack::flapjack_stack_builder::FlapJackStackBuilder;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(short, long)]
+    name: String,
+
+    #[clap(short, long, default_value_t = 1)]
+    count: u8,
+}
 
 fn main() {
-    file_io::init_log_db();
-    println!("{}", file_io::read_raw_db_contents().unwrap());
+    let path = file_io::init_log_db();
+    let content = file_io::read_raw_db_contents(&path).unwrap();
+
+    let stack = FlapJackStackBuilder::new(&content);
+    // todo write cli interface
 }
