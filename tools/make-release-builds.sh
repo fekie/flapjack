466a0117c371fc1cd70582c8ed37443ca830e35a
Chloe-Woahie <longwelldotpy@gmail.com>
2022-04-29 08:25:33 -0400

    switched to rustls

---
+++ tools/make-release-builds.sh
@@ -0,0 +1,15 @@
+if [ ! -f "Cargo.toml" ]; 
+then
+    echo "Please run this script in the root project directory!"
+    exit 1
+fi
+
+if [ ! -d target/custom-builds ]; then
+  mkdir -p target/custom-builds;
+fi
+
+cross build --release --target x86_64-unknown-linux-gnu
+cargo build --release
+
+7z a "./target/custom-builds/flapjack-v(VERSION)-x86_64-pc-windows-msvc.zip" ~/Git/flapjack/target/release/flapjack.exe
+7z a "./target/custom-builds/flapjack-v(VERSION)-x86_64-unknown-linux-gnu.tar.gz" ~/Git/flapjack/target/x86_64-unknown-linux-gnu/release/flapjack
