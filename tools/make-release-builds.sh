756ca783409906aaf8f0561f050eeac66823c2e8
Chloe-Woahie <longwelldotpy@gmail.com>
2022-05-11 13:03:11 -0400

    changed release tool to support raspberry pi 0

--- tools/make-release-builds.sh
+++ tools/make-release-builds.sh
@@ -10,10 +10,16 @@ if [ ! -d target/custom-builds ]; then
   mkdir -p target/custom-builds;
 fi
 
+rm -rf ./target/custom-builds/*
+
 cross build --release --target x86_64-unknown-linux-gnu
+cross build --release --target arm-unknown-linux-gnueabi
 cargo build --release
 
 7z a "./target/custom-builds/flapjack-${VERSION}-x86_64-pc-windows-msvc.zip" ~/Git/flapjack/target/release/flapjack.exe
 7z a "./target/custom-builds/flapjack-${VERSION}-x86_64-unknown-linux-gnu.tar" ~/Git/flapjack/target/x86_64-unknown-linux-gnu/release/flapjack
 7z a "./target/custom-builds/flapjack-${VERSION}-x86_64-unknown-linux-gnu.tar.gz" "./target/custom-builds/flapjack-${VERSION}-x86_64-unknown-linux-gnu.tar"
 rm -rf "./target/custom-builds/flapjack-${VERSION}-x86_64-unknown-linux-gnu.tar"
+7z a "./target/custom-builds/flapjack-${VERSION}-arm-unknown-linux-gnueabi.tar" ~/Git/flapjack/target/arm-unknown-linux-gnueabi/release/flapjack
+7z a "./target/custom-builds/flapjack-${VERSION}-arm-unknown-linux-gnueabi.tar.gz" "./target/custom-builds/flapjack-${VERSION}-arm-unknown-linux-gnueabi.tar"
+rm -rf "./target/custom-builds/flapjack-${VERSION}-arm-unknown-linux-gnueabi.tar"
