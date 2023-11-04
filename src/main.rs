d1dae352f7767947883db330ab495542a44d0fd5
Chloe-Woahie <68732833+Chloe-Woahie@users.noreply.github.com>
2023-11-04 01:09:24 -0400

    fix: fix program crashing when it can't update

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,18 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // We check to make sure that there is not a new update available.
     // This block of logic exits with an error if there is an error,
     // or exits the program if the user decides to update.
-    if updates::try_update()? {
-        eprintln!("Binary updated. Please restart the program!");
-        exit(0)
+    match updates::try_update() {
+        Ok(success) => {
+            if success {
+                eprintln!("Binary updated. Please restart the program!");
+                exit(0);
+            }
+        }
+        Err(_) => {
+            // We go ahead and let them continue. This can happen
+            // if a release does not have an update available.
+            eprintln!("Error trying to update. A precompiled version may not be available. Please update manually.")
+        }
     }
 
     let path = file_io::init_log_db()?;
